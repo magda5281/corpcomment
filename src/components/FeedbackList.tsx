4476a4caee986b55d23c32f95de910916c92a7f3
Magdalena Gozdalik <magdagozdalik28@gmail.com>
2025-05-03 22:51:04 +0100

    Add components , layouts, css stayls

---
+++ src/components/FeedbackList.tsx
@@ -0,0 +1,24 @@
+import { TriangleUpIcon } from '@radix-ui/react-icons';
+export default function FeedbackList() {
+  return (
+    <ol className='feedback-list'>
+      <li className='feedback'>
+        <button>
+          <TriangleUpIcon />
+          <span>593</span>
+        </button>
+        <div>
+          <p>B</p>
+        </div>
+        <div>
+          <p>Bytegrad</p>
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Labore
+            praesentium officia molestias dolore vel excepturi.
+          </p>
+        </div>
+        <p>4d</p>
+      </li>
+    </ol>
+  );
+}
---
+++ src/components/HashtagList.tsx
@@ -0,0 +1,15 @@
+export default function HashtagList() {
+  return (
+    <ul className='hashtags'>
+      <li>
+        <button className=''>#ByteGrad</button>
+      </li>
+      <li>
+        <button>#Nike</button>
+      </li>
+      <li>
+        <button>#McDonalds</button>
+      </li>
+    </ul>
+  );
+}
